e23bbf884c9030ffb4f3ddd66da5fe3a80ed298a
James Knight <james.d.knight@live.com>
2020-02-29 12:03:05 -0500

    __main__: allow users to query version
    Provide a mainline option to allow users query the explicit version of
    this theme.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ sphinx_alice_theme/__main__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 James Knight
+
+from . import __version__
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(prog='sphinx-alice-theme',
+        description='Alice Theme for Sphinx')
+    parser.add_argument(
+        '--version', action='version', version='%(prog)s ' + __version__)
+
+    parser.parse_args()
+    parser.print_help()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
