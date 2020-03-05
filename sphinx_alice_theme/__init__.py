702163739ecb4c629b926eb04cc8f854cc43f1b2
James Knight <james.d.knight@live.com>
2020-03-04 23:39:12 -0500

    superscript style citation references
    Adjust the theme to superscript citation references to have a similar
    feel to footnote references. To support styling these references, a
    transform has been added to mark links with inline nodes (for now,
    appears to be only citations) with the class `inline-link`; which in
    turn, will be superscript styled.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinx_alice_theme/__init__.py
+++ sphinx_alice_theme/__init__.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 # Copyright 2020 James Knight
 
+from docutils import nodes
 from os import path
+from sphinx.transforms.post_transforms import SphinxPostTransform
 
 __version__ = '0.1.0-dev0'
 
+class AliceThemeTransform(SphinxPostTransform):
+    default_priority = 400
+
+    def run(self, **kwargs):
+        for node in self.document.traverse(nodes.reference):
+            # tag references with `inline` child with a `inline-link` class
+            # (to style citation references)
+            if isinstance(next(iter(node.children), None), nodes.inline):
+                classes = node.get('classes', [])
+                classes.append('inline-link')
 def setup(app):
     app.add_html_theme('sphinx_alice_theme',
         path.abspath(path.dirname(__file__)))
+    app.add_post_transform(AliceThemeTransform)
 
     return {
         'parallel_read_safe': True,
