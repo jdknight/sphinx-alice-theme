60e89c4e2e3a62d5c1c9ae9d0a9d3d2d806c724c
James Knight <james.d.knight@live.com>
2021-11-23 11:45:12 -0500

    only iterate to next reference node once
    When performing reference-related node manipulation, only determine the
    "next" node once per reference, instead of possibly performing the same
    action twice.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinx_alice_theme/__init__.py
+++ sphinx_alice_theme/__init__.py
@@ -14,14 +14,16 @@ class AliceThemeTransform(SphinxPostTransform):
 
     def run(self, **kwargs):
         for node in self.document.traverse(nodes.reference):
+            next_node = next(iter(node.children), None)
+
             # tag references with `inline` child with a `inline-link` class
             # (to style citation references)
-            if isinstance(next(iter(node.children), None), nodes.inline):
+            if isinstance(next_node, nodes.inline):
                 classes = node.get('classes', [])
                 classes.append('inline-link')
             # tag references with `literal` child with a `literal-link` class
             # (to suppress hover styling)
-            elif isinstance(next(iter(node.children), None), nodes.literal):
+            elif isinstance(next_node, nodes.literal):
                 classes = node.get('classes', [])
                 classes.append('literal-link')
 
